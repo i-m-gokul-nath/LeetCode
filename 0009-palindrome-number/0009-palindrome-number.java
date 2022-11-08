108933ed307b3fb495481750fad71e32626e913d
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2022-11-08 16:07:17 +0530

    Time: 15 ms (76.28%), Space: 44 MB (76.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original = x;
+        int rev = 0;
+        while(x>0){
+            rev = x%10 + rev*10;
+            x= x/10;
+        }
+        return rev==original ? true : false;
+    }
+}
