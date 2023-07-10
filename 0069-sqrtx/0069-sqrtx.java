9043f0059049089fc3df893983eac21449786945
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-07-10 10:19:02 +0530

    Time: 1 ms (100.00%), Space: 40 MB (43.00%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        
+        long left = 1;
+        long right = x;
+        
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            
+            if (mid * mid == x) {
+                return (int) mid;
+            } else if (mid * mid < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return (int) right;
+    }
+}
