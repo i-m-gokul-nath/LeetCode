81c119cf7adea947b81ed485ea91b1658ed99849
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-07-20 11:37:36 +0530

    Time: 1 ms (99.65%), Space: 46.3 MB (90.99%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        result[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            
+            result[i] = result[i / 2] + (i % 2);
+        }
+
+        return result;
+    }
+}
