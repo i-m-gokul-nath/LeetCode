496777d8b549403e5cf4e952ca0f6fe21ccd4ea4
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-07-22 12:21:13 +0530

    Time: 9 ms (40.39%), Space: 44.6 MB (69.46%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length;
+        int n = img[0].length;
+        int[][] result = new int[m][n];
+
+        int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = img[i][j];
+                int count = 1;
+
+                for (int[] dir : directions) {
+                    int newRow = i + dir[0];
+                    int newCol = j + dir[1];
+
+                    if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
+                        sum += img[newRow][newCol];
+                        count++;
+                    }
+                }
+
+                result[i][j] = sum / count;
+            }
+        }
+
+        return result;
+    }
+}
