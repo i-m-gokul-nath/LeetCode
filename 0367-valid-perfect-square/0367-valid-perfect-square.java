2af3bcf9dc5c3d483e8245f2d5c399fd824dc04c
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-07-30 07:08:54 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (28.46%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num < 2) {
+            return true;
+        }
+
+        long left = 2;
+        long right = num / 2;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
