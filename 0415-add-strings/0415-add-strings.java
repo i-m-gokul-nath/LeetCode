1ae3fce3606469019255deccef3b2aea6100cffb
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-07-31 13:56:16 +0530

    Time: 1 ms (100.00%), Space: 41.5 MB (86.98%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += num1.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += num2.charAt(j) - '0';
+                j--;
+            }
+
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return result.reverse().toString();
+    }
+}
