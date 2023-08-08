964ddc1edc54a926506c133bc89d18e67bf970e7
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-08-08 10:00:55 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (89.09%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        String concatenated = s + s;
+        return concatenated.contains(goal);
+    }
+}
