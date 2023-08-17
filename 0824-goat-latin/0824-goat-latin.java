0cdb67eb5e1d7914e35e1ea7d51a2fcaf5ef63df
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-08-17 22:13:29 +0530

    Time: 1 ms (100.00%), Space: 40.7 MB (87.14%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.java
+++ 0824-goat-latin/0824-goat-latin.java
@@ -1,26 +1,29 @@
 class Solution {
     public String toGoatLatin(String sentence) {
-        String[] words = sentence.split(" ");
         StringBuilder result = new StringBuilder();
         String vowels = "aeiouAEIOU";
         String appendA = "a";
         
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            char firstChar = word.charAt(0);
-            
-            if (vowels.indexOf(firstChar) != -1) {
-                result.append(word).append("ma");
-            } else {
-                result.append(word.substring(1)).append(firstChar).append("ma");
+        int wordStart = 0;
+        for (int i = 0; i <= sentence.length(); i++) {
+            if (i == sentence.length() || sentence.charAt(i) == ' ') {
+                String word = sentence.substring(wordStart, i);
+                char firstChar = word.charAt(0);
+                
+                if (vowels.indexOf(firstChar) != -1) {
+                    result.append(word).append("ma");
+                } else {
+                    result.append(word.substring(1)).append(firstChar).append("ma");
+                }
+                
+                result.append(appendA);
+                if (i != sentence.length()) {
+                    result.append(" ");
+                }
+                
+                appendA += "a";
+                wordStart = i + 1;
             }
-            
-            result.append(appendA);
-            if (i != words.length - 1) {
-                result.append(" ");
-            }
-            
-            appendA += "a";
         }
         
         return result.toString();
