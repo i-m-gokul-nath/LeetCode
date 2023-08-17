5277b7405d3d05c8a410ba99c60be04dc97c5720
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-08-17 22:04:25 +0530

    Time: 12 ms (94.39%), Space: 50.8 MB (50.43%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+    private boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001]; // Initialize the array with a size of 10^6 + 1
+    }
+    
+    public void add(int key) {
+        set[key] = true;
+    }
+    
+    public void remove(int key) {
+        set[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
