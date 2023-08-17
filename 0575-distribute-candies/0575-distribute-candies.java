b0763dcb41540cd8551c9663a32d9787198bff2c
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-08-17 22:09:23 +0530

    Time: 29 ms (83.56%), Space: 44.9 MB (34.06%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,13 +1,11 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        Set<Integer> uniqueCandies = new HashSet<>();
+        Set<Integer> candySet = new HashSet<>();
         
         for (int candy : candyType) {
-            uniqueCandies.add(candy);
+            candySet.add(candy);
         }
         
-        int maxCandies = candyType.length / 2;
-        
-        return Math.min(uniqueCandies.size(), maxCandies);
+        return Math.min(candySet.size(), candyType.length / 2);
     }
 }
