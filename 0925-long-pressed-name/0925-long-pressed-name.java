1d1201171d220d91618998ee3fd8907f03b20427
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-08-24 16:07:02 +0530

    Time: 1 ms (69.95%), Space: 40.5 MB (68.98%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.java
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -6,10 +6,12 @@ class Solution {
         while (j < typed.length()) {
             if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
                 i++;
+                j++;
             } else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) {
                 return false;
+            } else {
+                j++;
             }
-            j++;
         }
         
         return i == name.length();
