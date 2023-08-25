81f23846cc1e878a78530b45c12141881493d3ce
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-08-25 09:20:07 +0530

    Time: 3 ms (79.03%), Space: 40.3 MB (87.03%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -3,32 +3,34 @@ import java.util.Stack;
 class Solution {
     public int calPoints(String[] operations) {
         Stack<Integer> stack = new Stack<>();
+        int totalSum = 0;
         
         for (String op : operations) {
             if (op.equals("C")) {
                 if (!stack.isEmpty()) {
-                    stack.pop(); // Remove the last score
+                    totalSum -= stack.pop(); // Remove and subtract the last score from the sum
                 }
             } else if (op.equals("D")) {
                 if (!stack.isEmpty()) {
                     int prevScore = stack.peek();
-                    stack.push(prevScore * 2); // Double the last score
+                    int newScore = prevScore * 2;
+                    stack.push(newScore); // Push the new score onto the stack
+                    totalSum += newScore; // Add the new score to the sum
                 }
             } else if (op.equals("+")) {
                 if (stack.size() >= 2) {
                     int lastScore = stack.pop();
                     int secondLastScore = stack.peek();
+                    int newScore = lastScore + secondLastScore;
                     stack.push(lastScore);
-                    stack.push(lastScore + secondLastScore); // Add the sum of the last two scores
+                    stack.push(newScore); // Push the new score onto the stack
+                    totalSum += newScore; // Add the new score to the sum
                 }
             } else {
-                stack.push(Integer.parseInt(op)); // Add the integer score to the stack
+                int score = Integer.parseInt(op);
+                stack.push(score); // Push the integer score onto the stack
+                totalSum += score; // Add the score to the sum
             }
-        }
-        
-        int totalSum = 0;
-        for (int score : stack) {
-            totalSum += score;
         }
         
         return totalSum;
