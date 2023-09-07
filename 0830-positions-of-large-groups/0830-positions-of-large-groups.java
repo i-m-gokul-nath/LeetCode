786cef00ff41f89c17beea1ff1c3529ca749034e
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-09-07 19:55:45 +0530

    Time: 2 ms (35.00%), Space: 43.9 MB (30.21%) - LeetHub

--- 0830-positions-of-large-groups/0830-positions-of-large-groups.java
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.java
@@ -2,29 +2,14 @@ class Solution {
     public List<List<Integer>> largeGroupPositions(String s) {
         List<List<Integer>> result = new ArrayList<>();
         int start = 0;
-        int end = 0;
         
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                end = i;
-            } else {
-                if (end - start >= 2) { // Large group with 3 or more characters
-                    List<Integer> interval = new ArrayList<>();
-                    interval.add(start);
-                    interval.add(end);
-                    result.add(interval);
+        for (int i = 0; i < s.length(); i++) {
+            if (i == s.length() - 1 || s.charAt(i) != s.charAt(i + 1)) {
+                if (i - start + 1 >= 3) {
+                    result.add(Arrays.asList(start, i));
                 }
-                start = i;
-                end = i;
+                start = i + 1;
             }
-        }
-        
-        // Check the last group
-        if (end - start >= 2) { // Large group with 3 or more characters
-            List<Integer> interval = new ArrayList<>();
-            interval.add(start);
-            interval.add(end);
-            result.add(interval);
         }
         
         return result;
