283e2fa6f9ad4abe80d32b95a11221f21e0eed22
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-09-16 09:51:26 +0530

    Time: 1 ms (99.99%), Space: 42.8 MB (99.30%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -2,15 +2,25 @@ class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
+        int[] count = new int[101]; // As per the constraint, 0 <= nums[i] <= 100
         
+        // Count the frequency of each number in nums
+        for (int num : nums) {
+            count[num]++;
+        }
+        
+        // Calculate the count of numbers smaller than each element in nums
+        for (int i = 1; i <= 100; i++) {
+            count[i] += count[i - 1];
+        }
+        
+        // Fill the result array
         for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (i != j && nums[j] < nums[i]) {
-                    count++;
-                }
+            if (nums[i] == 0) {
+                result[i] = 0; // Special case for 0
+            } else {
+                result[i] = count[nums[i] - 1];
             }
-            result[i] = count;
         }
         
         return result;
