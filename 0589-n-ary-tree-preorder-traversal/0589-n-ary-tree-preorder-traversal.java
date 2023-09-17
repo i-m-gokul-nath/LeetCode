4d3ac2371a5829b83b46ef5432224022db8e2dd9
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-09-17 09:25:13 +0530

    Time: 3 ms (25.54%), Space: 44.8 MB (11.92%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            result.add(node.val);
+
+            // Push children onto the stack in reverse order (right to left)
+            for (int i = node.children.size() - 1; i >= 0; i--) {
+                stack.push(node.children.get(i));
+            }
+        }
+
+        return result;
+    }
+}
