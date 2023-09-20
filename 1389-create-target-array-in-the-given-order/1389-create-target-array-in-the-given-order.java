0672c0911cf0eb46b35b47b12857c29f69b10ae9
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-09-20 09:35:09 +0530

    Time: 1 ms (25.76%), Space: 40.7 MB (87.48%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,21 +1,25 @@
+import java.util.LinkedList;
+
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
         int n = nums.length;
-        int[] target = new int[n];
+        LinkedList<Integer> target = new LinkedList<>();
         
         for (int i = 0; i < n; i++) {
             int idx = index[i];
             int num = nums[i];
             
-            // Shift elements to the right to make space for the new element
-            for (int j = n - 1; j > idx; j--) {
-                target[j] = target[j - 1];
-            }
-            
             // Insert the element at the specified index
-            target[idx] = num;
+            target.add(idx, num);
         }
         
-        return target;
+        // Convert the LinkedList to an array
+        int[] result = new int[n];
+        int i = 0;
+        for (int num : target) {
+            result[i++] = num;
+        }
+        
+        return result;
     }
 }
