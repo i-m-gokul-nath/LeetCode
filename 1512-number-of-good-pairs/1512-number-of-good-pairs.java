420a1a2829e61a538af16106737ca284458340b6
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-09-27 09:33:47 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (27.21%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        // Create a HashMap to store the count of each number
-        Map<Integer, Integer> countMap = new HashMap<>();
-        int goodPairs = 0;
+        int[] count = new int[101]; // Since 1 <= nums[i] <= 100
 
-        // Count the occurrences of each number in nums
         for (int num : nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            count[num]++;
         }
 
-        // Calculate the number of good pairs
-        for (int count : countMap.values()) {
-            goodPairs += count * (count - 1) / 2; // Using the combination formula
+        int goodPairs = 0;
+        for (int c : count) {
+            if (c > 1) {
+                goodPairs += (c * (c - 1)) / 2;
+            }
         }
 
         return goodPairs;
