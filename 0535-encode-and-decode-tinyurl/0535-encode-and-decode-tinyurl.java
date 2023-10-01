83abf7655e7ea146f8c4ae1ab254140c304c9d96
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-01 14:22:57 +0530

    Time: 2 ms (57.36%), Space: 41.7 MB (67.66%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,33 +1,24 @@
 import java.util.HashMap;
 
 public class Codec {
-    private HashMap<String, String> longToShort;
-    private HashMap<String, String> shortToLong;
+    private HashMap<String, String> urls;
     private static final String BASE_HOST = "http://tinyurl.com/";
     private int counter;
 
     public Codec() {
-        longToShort = new HashMap<>();
-        shortToLong = new HashMap<>();
+        urls = new HashMap<>();
         counter = 0;
     }
 
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        if (longToShort.containsKey(longUrl)) {
-            return BASE_HOST + longToShort.get(longUrl);
-        }
-
-        String shortKey = String.valueOf(counter++);
-        longToShort.put(longUrl, shortKey);
-        shortToLong.put(shortKey, longUrl);
-
-        return BASE_HOST + shortKey;
+        String shortKey = BASE_HOST + String.valueOf(counter++);
+        urls.put(shortKey, longUrl);
+        return shortKey;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        String shortKey = shortUrl.replace(BASE_HOST, "");
-        return shortToLong.getOrDefault(shortKey, "");
+        return urls.getOrDefault(shortUrl, "");
     }
 }
