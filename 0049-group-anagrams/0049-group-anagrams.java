cfb429167a1978842b002f9721dfa29ad913e867
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-08 10:59:34 +0530

    Time: 5 ms (98.87%), Space: 46.6 MB (82.53%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+        
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            
+            if (!anagramGroups.containsKey(sortedStr)) {
+                anagramGroups.put(sortedStr, new ArrayList<>());
+            }
+            
+            anagramGroups.get(sortedStr).add(str);
+        }
+        
+        return new ArrayList<>(anagramGroups.values());
+    }
+}
