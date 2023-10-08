63dc1168f04034a26eeaaa839b330d1b3031c17e
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-08 11:03:03 +0530

    Time: 5 ms (98.87%), Space: 46.5 MB (89.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,36 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return new ArrayList<>();
-        }
-
         Map<String, List<String>> anagramGroups = new HashMap<>();
 
         for (String str : strs) {
-            int[] charCount = new int[26];
-            for (char c : str.toCharArray()) {
-                charCount[c - 'a']++;
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+
+            if (!anagramGroups.containsKey(sortedStr)) {
+                anagramGroups.put(sortedStr, new ArrayList<>());
             }
 
-            StringBuilder sortedStr = new StringBuilder();
-            for (int i = 0; i < 26; i++) {
-                if (charCount[i] > 0) {
-                    sortedStr.append((char) ('a' + i)).append(charCount[i]);
-                }
-            }
-
-            String key = sortedStr.toString();
-
-            if (!anagramGroups.containsKey(key)) {
-                anagramGroups.put(key, new ArrayList<>());
-            }
-
-            anagramGroups.get(key).add(str);
+            anagramGroups.get(sortedStr).add(str);
         }
 
         return new ArrayList<>(anagramGroups.values());
