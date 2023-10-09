251ca42781e9af3f6518456a01c275646e80462f
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-09 13:28:21 +0530

    Time: 7 ms (89.22%), Space: 44.9 MB (8.43%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -1,19 +1,19 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
-       
+        
         Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
         
-        
-        List<int[]> queue = new LinkedList<>();
-        
+        List<int[]> queue = new ArrayList<>();
         
         for (int[] person : people) {
             queue.add(person[1], person);
         }
         
-        
+        // Convert the ArrayList to an array
         return queue.toArray(new int[people.length][2]);
     }
 }
