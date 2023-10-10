4c2871cf412bc8595ec76803a648c4c2259aeb26
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-10 19:47:22 +0530

    Time: 12 ms (80.28%), Space: 44.3 MB (75.85%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -8,13 +8,16 @@ class Solution {
             charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
         }
 
-        // Create a list of characters sorted by frequency in decreasing order
-        List<Character> sortedChars = new ArrayList<>(charFrequency.keySet());
-        sortedChars.sort((a, b) -> charFrequency.get(b) - charFrequency.get(a));
+        // Create a max-heap (priority queue) based on character frequency
+        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> charFrequency.get(b) - charFrequency.get(a));
 
-        // Build the result string based on the sorted characters and their frequency
+        // Add all characters to the max-heap
+        maxHeap.addAll(charFrequency.keySet());
+
+        // Build the result string based on the max-heap
         StringBuilder result = new StringBuilder();
-        for (char c : sortedChars) {
+        while (!maxHeap.isEmpty()) {
+            char c = maxHeap.poll();
             int frequency = charFrequency.get(c);
             for (int i = 0; i < frequency; i++) {
                 result.append(c);
