f5a12319eb43bba9c2fdc8563022e9a403ab5d2b
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-16 13:32:43 +0530

    Time: 5 ms (11.64%), Space: 44.2 MB (5.65%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,21 +1,38 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        generate(result, "", 0, 0, n);
+        if (n == 0) {
+            return result;
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("(");
+        Queue<Integer> openCount = new LinkedList<>();
+        Queue<Integer> closeCount = new LinkedList();
+        openCount.offer(1);
+        closeCount.offer(0);
+
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+            int open = openCount.poll();
+            int close = closeCount.poll();
+
+            if (open == n && close == n) {
+                result.add(current);
+            }
+
+            if (open < n) {
+                queue.offer(current + "(");
+                openCount.offer(open + 1);
+                closeCount.offer(close);
+            }
+            if (close < open) {
+                queue.offer(current + ")");
+                openCount.offer(open);
+                closeCount.offer(close + 1);
+            }
+        }
+        
         return result;
-    }
-
-    private void generate(List<String> result, String current, int open, int close, int max) {
-        if (current.length() == max * 2) {
-            result.add(current);
-            return;
-        }
-
-        if (open < max) {
-            generate(result, current + "(", open + 1, close, max);
-        }
-        if (close < open) {
-            generate(result, current + ")", open, close + 1, max);
-        }
     }
 }
