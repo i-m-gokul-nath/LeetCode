73c0e7fdbff2717e770e25e859383bf5d1d2e4a6
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-26 08:25:10 +0530

    Time: 5 ms (60.46%), Space: 44.3 MB (43.49%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s; // If numRows is 1, return the original string
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(currentRow).append(c);
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
