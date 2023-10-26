a29dd462ac3ec242247488a38237877213e93707
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-26 08:27:12 +0530

    Time: 2 ms (99.78%), Space: 43.5 MB (88.54%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,28 +1,21 @@
 class Solution {
     public String convert(String s, int numRows) {
         if (numRows == 1) return s; // If numRows is 1, return the original string
+        
+        int n = s.length();
+        char[] result = new char[n];
+        int cycleLen = 2 * numRows - 2;
+        int index = 0;
 
-        List<StringBuilder> rows = new ArrayList<>();
-        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
-            rows.add(new StringBuilder());
-        }
-
-        int currentRow = 0;
-        boolean goingDown = false;
-
-        for (char c : s.toCharArray()) {
-            rows.get(currentRow).append(c);
-            if (currentRow == 0 || currentRow == numRows - 1) {
-                goingDown = !goingDown;
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; i + j < n; j += cycleLen) {
+                result[index++] = s.charAt(i + j);
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                    result[index++] = s.charAt(j + cycleLen - i);
+                }
             }
-            currentRow += goingDown ? 1 : -1;
         }
 
-        StringBuilder result = new StringBuilder();
-        for (StringBuilder row : rows) {
-            result.append(row);
-        }
-
-        return result.toString();
+        return new String(result);
     }
 }
