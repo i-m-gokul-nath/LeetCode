dfa15d7fa5c9494a3095ac51c089dcb4389162ba
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-10-28 10:17:13 +0530

    Time: 14 ms (35.26%), Space: 44.9 MB (37.42%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,32 +1,35 @@
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        // Convert time points to minutes
+        int n = timePoints.size();
         List<Integer> minutes = new ArrayList<>();
+        
         for (String time : timePoints) {
-            String[] parts = time.split(":");
-            int h = Integer.parseInt(parts[0]);
-            int m = Integer.parseInt(parts[1]);
-            minutes.add(h * 60 + m);
+            int minutesOfDay = convertToMinutes(time);
+            minutes.add(minutesOfDay);
         }
-
-        // Sort the minutes
+        
         Collections.sort(minutes);
-
-        int minDifference = Integer.MAX_VALUE;
-
-        // Find the minimum difference between adjacent time points
-        for (int i = 1; i < minutes.size(); i++) {
-            int diff = minutes.get(i) - minutes.get(i - 1);
-            minDifference = Math.min(minDifference, diff);
+        
+        int minDiff = Integer.MAX_VALUE;
+        
+        for (int i = 1; i < n; i++) {
+            minDiff = Math.min(minDiff, minutes.get(i) - minutes.get(i - 1));
         }
-
+        
         // Consider the circular property of the 24-hour clock
-        int circularDifference = minutes.get(0) + (1440 - minutes.get(minutes.size() - 1));
-        return Math.min(minDifference, circularDifference);
+        int lastToFirst = 24 * 60 - minutes.get(n - 1) + minutes.get(0);
+        minDiff = Math.min(minDiff, lastToFirst);
+        
+        return minDiff;
+    }
+
+    private int convertToMinutes(String time) {
+        String[] parts = time.split(":");
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1]);
+        return hours * 60 + minutes;
     }
 }
