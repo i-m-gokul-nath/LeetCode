5a9b347a2b15fe621cb5aad0bd934bba68e4e869
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-11-01 09:54:09 +0530

    Time: 3 ms (96.44%), Space: 44 MB (50.31%) - LeetHub

--- 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
+++ 0508-most-frequent-subtree-sum/0508-most-frequent-subtree-sum.java
@@ -21,17 +21,13 @@ import java.util.Map;
 class Solution {
     public int[] findFrequentTreeSum(TreeNode root) {
         Map<Integer, Integer> sumFrequency = new HashMap<>();
-        int maxFrequency = 0;
+        int[] maxFrequency = new int[]{0};
 
-        calculateSubtreeSums(root, sumFrequency);
-
-        for (int frequency : sumFrequency.values()) {
-            maxFrequency = Math.max(maxFrequency, frequency);
-        }
+        calculateSubtreeSums(root, sumFrequency, maxFrequency);
 
         List<Integer> frequentSums = new ArrayList<>();
         for (Map.Entry<Integer, Integer> entry : sumFrequency.entrySet()) {
-            if (entry.getValue() == maxFrequency) {
+            if (entry.getValue() == maxFrequency[0]) {
                 frequentSums.add(entry.getKey());
             }
         }
@@ -44,17 +40,19 @@ class Solution {
         return result;
     }
 
-    private int calculateSubtreeSums(TreeNode node, Map<Integer, Integer> sumFrequency) {
+    private int calculateSubtreeSums(TreeNode node, Map<Integer, Integer> sumFrequency, int[] maxFrequency) {
         if (node == null) {
             return 0;
         }
 
-        int leftSum = calculateSubtreeSums(node.left, sumFrequency);
-        int rightSum = calculateSubtreeSums(node.right, sumFrequency);
+        int leftSum = calculateSubtreeSums(node.left, sumFrequency, maxFrequency);
+        int rightSum = calculateSubtreeSums(node.right, sumFrequency, maxFrequency);
 
         int currentSum = leftSum + rightSum + node.val;
 
         sumFrequency.put(currentSum, sumFrequency.getOrDefault(currentSum, 0) + 1);
+
+        maxFrequency[0] = Math.max(maxFrequency[0], sumFrequency.get(currentSum));
 
         return currentSum;
     }
