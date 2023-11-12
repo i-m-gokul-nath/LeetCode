994c2cb017df9098bc7f0403d52fb7e8acdb2671
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-11-12 12:55:08 +0530

    Time: 16 ms (61.99%), Space: 43.3 MB (45.10%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,17 +1,22 @@
+import java.util.Arrays;
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
 
+        Arrays.sort(coins);
+
         for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i) {
+            for (int j = coins.length - 1; j >= 0; j--) {
+                int coin = coins[j];
+                if (coin <= i && dp[i - coin] != Integer.MAX_VALUE) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
