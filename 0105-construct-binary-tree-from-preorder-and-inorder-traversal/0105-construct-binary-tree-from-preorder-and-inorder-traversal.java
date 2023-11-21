6170e2af066aff891091b24f6922461255ebb210
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-11-21 09:01:23 +0530

    Time: 3 ms (44.78%), Space: 43.6 MB (29.73%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,26 +15,25 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> inorderMap = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
-            inorderMap.put(inorder[i], i);
-        }
-        return buildTreeHelper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inorderMap);
+        return buildTreeHelper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
 
-    private TreeNode buildTreeHelper(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inorderMap) {
+    private TreeNode buildTreeHelper(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
         if (preStart > preEnd || inStart > inEnd) {
             return null;
         }
 
-        int rootVal = preorder[preStart];
-        TreeNode root = new TreeNode(rootVal);
+        TreeNode root = new TreeNode(preorder[preStart]);
 
-        int rootIndexInorder = inorderMap.get(rootVal);
+        int rootIndexInorder = inStart;
+        while (inorder[rootIndexInorder] != preorder[preStart]) {
+            rootIndexInorder++;
+        }
+
         int leftSubtreeSize = rootIndexInorder - inStart;
 
-        root.left = buildTreeHelper(preorder, preStart + 1, preStart + leftSubtreeSize, inorder, inStart, rootIndexInorder - 1, inorderMap);
-        root.right = buildTreeHelper(preorder, preStart + leftSubtreeSize + 1, preEnd, inorder, rootIndexInorder + 1, inEnd, inorderMap);
+        root.left = buildTreeHelper(preorder, preStart + 1, preStart + leftSubtreeSize, inorder, inStart, rootIndexInorder - 1);
+        root.right = buildTreeHelper(preorder, preStart + leftSubtreeSize + 1, preEnd, inorder, rootIndexInorder + 1, inEnd);
 
         return root;
     }
