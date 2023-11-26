7cd4e4fd15782862d02fa2399f828520a87712a9
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2023-11-26 15:01:12 +0530

    Time: 1 ms (97.82%), Space: 44.4 MB (5.52%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -13,56 +13,40 @@
  *     }
  * }
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
+    private Map<Integer, Integer> inorderMap;
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        // Check if the input arrays are empty
+        
         if (inorder == null || postorder == null || inorder.length == 0 || postorder.length == 0) {
             return null;
         }
-        
-        // Build the tree using recursive helper function
+
+        inorderMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+
         return buildTreeHelper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
-    
+
     private TreeNode buildTreeHelper(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
-        // Base case
+        
         if (inStart > inEnd || postStart > postEnd) {
             return null;
         }
-        
-        // The last element in postorder is the root of the current subtree
+
         int rootValue = postorder[postEnd];
         TreeNode root = new TreeNode(rootValue);
-        
-        // Find the index of the root value in the inorder array
-        int rootIndexInorder = 0;
-        for (int i = inStart; i <= inEnd; i++) {
-            if (inorder[i] == rootValue) {
-                rootIndexInorder = i;
-                break;
-            }
-        }
-        
-        // Build the right and left subtrees recursively
+
+        int rootIndexInorder = inorderMap.get(rootValue);
+
         root.left = buildTreeHelper(inorder, inStart, rootIndexInorder - 1, postorder, postStart, postStart + (rootIndexInorder - inStart) - 1);
         root.right = buildTreeHelper(inorder, rootIndexInorder + 1, inEnd, postorder, postStart + (rootIndexInorder - inStart), postEnd - 1);
-        
+
         return root;
     }
 }
