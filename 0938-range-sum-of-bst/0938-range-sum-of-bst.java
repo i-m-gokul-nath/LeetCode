12ee959206497074072ebbdd8ad1af269609dc98
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2024-01-08 09:44:36 +0530

    Time: 0 ms (100.00%), Space: 50.9 MB (23.62%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -20,6 +20,7 @@ class Solution {
         }
         
         int sum = 0;
+
         if (root.val >= low && root.val <= high) {
             sum += root.val;
         }
@@ -27,7 +28,6 @@ class Solution {
         if (root.val > low) {
             sum += rangeSumBST(root.left, low, high);
         }
-        
         if (root.val < high) {
             sum += rangeSumBST(root.right, low, high);
         }
