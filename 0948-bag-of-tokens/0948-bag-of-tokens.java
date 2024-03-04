46321ea4e4b5e7580189fd693783ef6c393ac5e5
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2024-03-04 18:18:03 +0530

    Time: 2 ms (97.13%), Space: 43.1 MB (14.97%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int left = 0, right = tokens.length - 1;
+        int score = 0, maxScore = 0;
+
+        while (left <= right) {
+            if (power >= tokens[left]) {
+                power -= tokens[left++];
+                score++;
+                maxScore = Math.max(maxScore, score);
+            } else if (score > 0) {
+                power += tokens[right--];
+                score--;
+            } else {
+                break;
+            }
+        }
+
+        return maxScore;
+    }
+}
