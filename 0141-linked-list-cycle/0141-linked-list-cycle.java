e45905b5e052a55b2f808b0d067ef8be4fe80858
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2024-03-06 10:00:53 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (78.40%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,25 +11,21 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // Check if the linked list is empty or contains only one node
         if (head == null || head.next == null) {
             return false;
         }
-        
+
         ListNode slow = head;
         ListNode fast = head.next;
-        
+
         while (slow != fast) {
-            // If the fast pointer reaches the end of the list, there is no cycle
             if (fast == null || fast.next == null) {
                 return false;
             }
-            
             slow = slow.next;
             fast = fast.next.next;
         }
-        
-        // If the loop exits, a cycle is detected
+
         return true;
     }
 }
