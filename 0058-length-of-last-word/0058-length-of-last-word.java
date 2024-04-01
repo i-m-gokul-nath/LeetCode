8dc0b71d3e9304e264a17d0f846de21011d809c9
Gokul Nath <70213718+i-m-gokul-nath@users.noreply.github.com>
2024-04-01 06:46:52 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (18.81%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim(); // Remove leading and trailing spaces
-        int lastSpaceIndex = s.lastIndexOf(' '); // Find the last space index
-        return s.length() - lastSpaceIndex - 1; // Calculate the length of the last word
+        int length = 0;
+        int i = s.length() - 1;
+        
+        while (i >= 0 && s.charAt(i) == ' ')
+            i--;
+        
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        
+        return length;
     }
 }
